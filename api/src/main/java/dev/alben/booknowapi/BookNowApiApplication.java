5355da2d36d17e2c78c4ad1d74ea3f5b658cfd26
Alben Bustamante <albenbustamante@gmail.com>
2025-07-12 16:37:56 -0500

    initial commit

---
+++ api/src/main/java/dev/alben/booknowapi/BookNowApiApplication.java
@@ -0,0 +1,13 @@
+package dev.alben.booknowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNowApiApplication.class, args);
+	}
+
+}
