f93a5d919bd519ad12521f9fba965f67536e3552
Alben Bustamante <albenbustamante@gmail.com>
2025-07-21 20:23:39 -0500

    design: custom button

---
+++ client/src/app/components/custom-button/custom-button.component.ts
@@ -0,0 +1,24 @@
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-button',
+  imports: [NgClass],
+  templateUrl: './custom-button.component.html',
+  styleUrl: './custom-button.component.css',
+})
+export class CustomButtonComponent {
+  @Input() color: 'primary' = 'primary';
+  @Input() type: 'submit' | 'button' = 'button';
+  @Input() disabled: boolean = false;
+  @Input() loading: boolean = false;
+
+  private colorMap = {
+    primary:
+      'bg-primary-500 hover:bg-primary-600 active:bg-primary-700 text-primary-50',
+  };
+
+  get getColor() {
+    return this.colorMap[this.color];
+  }
+}
--- client/src/app/features/auth/log-in/log-in.component.ts
+++ client/src/app/features/auth/log-in/log-in.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CustomInputComponent } from '../../../components/custom-input/custom-input.component';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CustomButtonComponent } from '../../../components/custom-button/custom-button.component';
 
 @Component({
   selector: 'app-log-in',
-  imports: [CustomInputComponent, ReactiveFormsModule],
+  imports: [CustomInputComponent, ReactiveFormsModule, CustomButtonComponent],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.css',
 })
