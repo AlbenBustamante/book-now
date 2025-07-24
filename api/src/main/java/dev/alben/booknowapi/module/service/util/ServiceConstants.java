87382b3c00a65f7ece5043a85a3570e5ee057ac3
Alben Bustamante <albenbustamante@gmail.com>
2025-07-24 17:08:22 -0500

    define: entity model and constants for services

---
+++ api/src/main/java/dev/alben/booknowapi/module/service/util/ServiceConstants.java
@@ -0,0 +1,15 @@
+package dev.alben.booknowapi.module.service.util;
+
+/**
+ * Constants for services.
+ */
+public final class ServiceConstants {
+    /**
+     * Max Length for service's name.
+     */
+    public static final int NAME_LENGTH = 70;
+    /**
+     * Max Length for service's detailed description.
+     */
+    public static final int DESCRIPTION_LENGTH = 620;
+}
