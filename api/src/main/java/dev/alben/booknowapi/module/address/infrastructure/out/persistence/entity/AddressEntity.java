8abc1fc989b8989baeeb499598c9b430b1f637a9
Alben Bustamante <albenbustamante@gmail.com>
2025-07-24 11:04:04 -0500

    define: entity model and constants for addresses

---
+++ api/src/main/java/dev/alben/booknowapi/module/address/infrastructure/out/persistence/entity/AddressEntity.java
@@ -0,0 +1,58 @@
+package dev.alben.booknowapi.module.address.infrastructure.out.persistence.entity;
+
+import dev.alben.booknowapi.core.auditable.AuditableEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+
+import static dev.alben.booknowapi.module.address.util.AddressConstants.CITY_LENGTH;
+import static dev.alben.booknowapi.module.address.util.AddressConstants.COUNTRY_LENGTH;
+import static dev.alben.booknowapi.module.address.util.AddressConstants.DIRECTION_LENGTH;
+import static dev.alben.booknowapi.module.address.util.AddressConstants.STATE_LENGTH;
+
+/**
+ * Entity model for Addresses.
+ */
+@Setter
+@Getter
+@Entity
+@Table(name = "addresses")
+@SQLDelete(sql = "UPDATE addresses SET delete_at = NOW() WHERE id = ?")
+public class AddressEntity extends AuditableEntity {
+    /**
+     * Auto generated ID.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * Country name.
+     */
+    @Column(nullable = false, length = COUNTRY_LENGTH)
+    private String country;
+
+    /**
+     * State name.
+     */
+    @Column(nullable = false, length = STATE_LENGTH)
+    private String state;
+
+    /**
+     * City name.
+     */
+    @Column(nullable = false, length = CITY_LENGTH)
+    private String city;
+
+    /**
+     * Full detailed address.
+     */
+    @Column(nullable = false, length = DIRECTION_LENGTH)
+    private String direction;
+}
---
+++ api/src/main/java/dev/alben/booknowapi/module/address/util/AddressConstants.java
@@ -0,0 +1,23 @@
+package dev.alben.booknowapi.module.address.util;
+
+/**
+ * Constants for addresses.
+ */
+public final class AddressConstants {
+    /**
+     * Max Length for country's name.
+     */
+    public static final int COUNTRY_LENGTH = 70;
+    /**
+     * Max Length for state's name.
+     */
+    public static final int STATE_LENGTH = 60;
+    /**
+     * Max Length for city's name.
+     */
+    public static final int CITY_LENGTH = 163;
+    /**
+     * Full Length for full detailed direction.
+     */
+    public static final int DIRECTION_LENGTH = 100;
+}
