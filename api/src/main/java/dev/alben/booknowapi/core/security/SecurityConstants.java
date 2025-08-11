ad1be6edd7bdd5cd4bbf15226e3bc5e9b744dc57
Alben Bustamante <albenbustamante@gmail.com>
2025-08-10 21:19:15 -0500

    add: docs with swagger

--- api/src/main/java/dev/alben/booknowapi/core/security/SecurityConstants.java
+++ api/src/main/java/dev/alben/booknowapi/core/security/SecurityConstants.java
@@ -3,6 +3,10 @@ package dev.alben.booknowapi.core.security;
 public final class SecurityConstants {
     public static final String[] WHITE_LIST = {
             "/auth",
-            "/auth/**"
+            "/auth/**",
+            "/swagger-ui",
+            "/swagger-ui/**",
+            "/v3",
+            "/v3/**"
     };
 }
