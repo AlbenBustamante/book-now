79c62239d0f14479401547f2e1602a1895023e9d
Alben Bustamante <albenbustamante@gmail.com>
2025-09-10 15:57:48 -0500

    add: side navigation bar to account layout

--- client/src/app/layouts/account-layout/account-layout.component.ts
+++ client/src/app/layouts/account-layout/account-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideNavbarComponent } from './components/side-navbar/side-navbar.component';
 
 @Component({
   selector: 'app-account-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideNavbarComponent],
   templateUrl: './account-layout.component.html',
   styleUrl: './account-layout.component.css',
 })
