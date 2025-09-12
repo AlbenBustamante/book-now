d4e8fc0bff4527c3d0d98f8cc8eaf62da5614096
Alben Bustamante <albenbustamante@gmail.com>
2025-09-12 17:20:12 -0500

    remove: unnecessary line

--- api/src/main/java/dev/alben/booknowapi/BookNowApiApplication.java
+++ api/src/main/java/dev/alben/booknowapi/BookNowApiApplication.java
@@ -8,7 +8,6 @@ public class BookNowApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookNowApiApplication.class, args);
-        new StringBuilder().reverse().toString();
     }
 
 }
