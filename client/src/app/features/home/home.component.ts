1c9862692573dbbb8cef5ba051b0a35b3a90407f
Alben Bustamante <albenbustamante@gmail.com>
2025-09-13 19:20:34 -0500

    feat: new service cards to show featured services at home page

--- client/src/app/features/home/home.component.ts
+++ client/src/app/features/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ServiceCardComponent } from './service-card/service-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ServiceCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
