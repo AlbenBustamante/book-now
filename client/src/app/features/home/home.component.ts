334e98c4543d049889b98ba3694db8e485ae0a5c
Alben Bustamante <albenbustamante@gmail.com>
2025-10-14 18:34:23 -0500

    feat: new top-rated professionals section

--- client/src/app/features/home/home.component.ts
+++ client/src/app/features/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import { ServiceCardComponent } from './service-card/service-card.component';
+import { ServiceCardComponent } from './components/service-card/service-card.component';
 import { ButtonComponent } from '@components/button/button.component';
 import { SubtitleComponent } from '@components/subtitle/subtitle.component';
+import { ProviderCardComponent } from './components/provider-card/provider-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ServiceCardComponent, ButtonComponent, SubtitleComponent],
+  imports: [
+    ServiceCardComponent,
+    ButtonComponent,
+    SubtitleComponent,
+    ProviderCardComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
