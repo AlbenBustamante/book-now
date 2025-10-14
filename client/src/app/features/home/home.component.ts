79dd4e9ac4e8968e5c51f0177ccd127c2a810ad3
Alben Bustamante <albenbustamante@gmail.com>
2025-10-13 22:57:39 -0500

    update: layout; design: home

--- client/src/app/features/home/home.component.ts
+++ client/src/app/features/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ServiceCardComponent } from './service-card/service-card.component';
+import { ButtonComponent } from '@components/button/button.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ServiceCardComponent],
+  imports: [ServiceCardComponent, ButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
