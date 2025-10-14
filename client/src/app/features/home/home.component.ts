2ffc46cf66920175900d19fbfa02962cf113637d
Alben Bustamante <albenbustamante@gmail.com>
2025-10-14 18:25:49 -0500

    update: service-card; define: subtitle component

--- client/src/app/features/home/home.component.ts
+++ client/src/app/features/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ServiceCardComponent } from './service-card/service-card.component';
 import { ButtonComponent } from '@components/button/button.component';
+import { SubtitleComponent } from '@components/subtitle/subtitle.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ServiceCardComponent, ButtonComponent],
+  imports: [ServiceCardComponent, ButtonComponent, SubtitleComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
--- client/src/app/features/home/service-card/service-card.component.ts
+++ client/src/app/features/home/service-card/service-card.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from '@components/button/button.component';
 
 @Component({
   selector: 'app-service-card',
-  imports: [ButtonComponent],
+  imports: [],
   templateUrl: './service-card.component.html',
   styleUrl: './service-card.component.css',
 })
