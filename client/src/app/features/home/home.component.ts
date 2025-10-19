01f9cb999ee24a54b6b4442952df8e47da3938ff
Alben Bustamante <albenbustamante@gmail.com>
2025-10-19 16:02:00 -0500

    refactor: home header into subcomponent; define: form for home searching input

--- client/src/app/features/home/home.component.ts
+++ client/src/app/features/home/home.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from '@components/button/button.component';
 import { SubtitleComponent } from '@components/subtitle/subtitle.component';
 import { ProviderCardComponent } from './components/provider-card/provider-card.component';
 import { ServicesCarouselComponent } from '@components/services-carousel/services-carousel.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-home',
   imports: [
-    ButtonComponent,
     SubtitleComponent,
     ProviderCardComponent,
     ServicesCarouselComponent,
+    HeaderComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
