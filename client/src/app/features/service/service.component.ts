dd0a112b4edd39f58605d67b3672351ff47e6d64
Alben Bustamante <albenbustamante@gmail.com>
2025-10-19 16:41:29 -0500

    refactor: reviews into a unique component

--- client/src/app/features/service/service.component.ts
+++ client/src/app/features/service/service.component.ts
@@ -3,9 +3,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { DescriptionComponent } from './components/description/description.component';
 import { AboutProviderComponent } from './components/about-provider/about-provider.component';
 import { DivisorComponent } from '@components/divisor/divisor.component';
-import { ValorationComponent } from './components/valoration/valoration.component';
-import { ReviewsComponent } from './components/reviews/reviews.component';
 import { ContainerComponent } from '@components/container/container.component';
+import { ReviewsComponent } from '@components/reviews/reviews.component';
 
 @Component({
   selector: 'app-service',
@@ -14,9 +13,8 @@ import { ContainerComponent } from '@components/container/container.component';
     DescriptionComponent,
     AboutProviderComponent,
     DivisorComponent,
-    ValorationComponent,
-    ReviewsComponent,
     ContainerComponent,
+    ReviewsComponent,
   ],
   templateUrl: './service.component.html',
   styleUrl: './service.component.css',
