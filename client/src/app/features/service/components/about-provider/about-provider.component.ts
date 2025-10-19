74677f267bfecd50674165e8dfb4cb2a69634206
Alben Bustamante <albenbustamante@gmail.com>
2025-10-19 12:19:09 -0500

    update: header and description styles; add: section for about the provider

---
+++ client/src/app/features/service/components/about-provider/about-provider.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SubtitleComponent } from '@components/subtitle/subtitle.component';
+
+@Component({
+  selector: 'app-about-provider',
+  imports: [SubtitleComponent],
+  templateUrl: './about-provider.component.html',
+  styleUrl: './about-provider.component.css',
+})
+export class AboutProviderComponent {}
--- client/src/app/features/service/service.component.ts
+++ client/src/app/features/service/service.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { DescriptionComponent } from './components/description/description.component';
+import { AboutProviderComponent } from './components/about-provider/about-provider.component';
+import { DivisorComponent } from '@components/divisor/divisor.component';
 
 @Component({
   selector: 'app-service',
-  imports: [HeaderComponent, DescriptionComponent],
+  imports: [
+    HeaderComponent,
+    DescriptionComponent,
+    AboutProviderComponent,
+    DivisorComponent,
+  ],
   templateUrl: './service.component.html',
   styleUrl: './service.component.css',
 })
