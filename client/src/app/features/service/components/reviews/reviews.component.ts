700fcd027ee1ca5b15f1515ec760a6ce41a0afa1
Alben Bustamante <albenbustamante@gmail.com>
2025-10-19 16:14:34 -0500

    move: comment component to shared components

--- client/src/app/features/service/components/reviews/reviews.component.ts
+++ client/src/app/features/service/components/reviews/reviews.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { SubtitleComponent } from '@components/subtitle/subtitle.component';
-import { CommentComponent } from '../comment/comment.component';
+import { CommentComponent } from '@components/comment/comment.component';
 
 @Component({
   selector: 'app-reviews',
