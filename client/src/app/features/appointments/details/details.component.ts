2e4a1cdd35b00a028681897dc0f7f8acf267578c
Alben Bustamante <albenbustamante@gmail.com>
2025-10-20 15:14:41 -0500

    link: appointments overview with details

--- client/src/app/features/appointments/details/details.component.ts
+++ client/src/app/features/appointments/details/details.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ContainerComponent } from '@components/container/container.component';
+import { TitleComponent } from '@components/title/title.component';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [ContainerComponent, TitleComponent],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css',
 })
--- client/src/app/features/appointments/overview/components/appointment/appointment.component.ts
+++ client/src/app/features/appointments/overview/components/appointment/appointment.component.ts
@@ -1,4 +1,5 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { Router } from '@angular/router';
 import { ButtonComponent } from '@components/button/button.component';
 
 @Component({
@@ -8,5 +9,10 @@ import { ButtonComponent } from '@components/button/button.component';
   styleUrl: './appointment.component.css',
 })
 export class AppointmentComponent {
+  private readonly _router = inject(Router);
   readonly time = input.required<'past' | 'next'>();
+
+  goToDetails() {
+    this._router.navigateByUrl('/appointments/details');
+  }
 }
