6bfd8183204e23bc4a16c2010da5ed0038e899d3
Alben Bustamante <albenbustamante@gmail.com>
2025-10-22 20:56:30 -0500

    feat: manager mobile navbar and navigation with children pages

--- client/src/app/features/manager/manager.routes.ts
+++ client/src/app/features/manager/manager.routes.ts
@@ -1,8 +1,28 @@
 import { Routes } from '@angular/router';
+import ManagerComponent from './manager.component';
 
 export default [
   {
     path: '',
-    loadComponent: () => import('./manager.component'),
+    component: ManagerComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'dashboard',
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./dashboard/dashboard.component'),
+      },
+      {
+        path: 'services',
+        loadComponent: () => import('./services/services.component'),
+      },
+      {
+        path: 'customers',
+        loadComponent: () => import('./customers/customers.component'),
+      },
+    ],
   },
 ] as Routes;
