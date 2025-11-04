307474ed5e3e0b3b601fb79f66b869c1d46d9a15
Alben Bustamante <albenbustamante@gmail.com>
2025-11-04 15:25:47 -0500

    update: settings design

--- client/src/app/features/settings/settings.component.ts
+++ client/src/app/features/settings/settings.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from "@components/container/container.component";
 
 @Component({
   selector: 'app-settings',
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet, ContainerComponent],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css',
 })
