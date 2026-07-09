dbfe76f0b3a57cd46f64101ca159fe9138701d77
Alben Bustamante <albenbustamante@gmail.com>
2026-07-09 12:47:43 -0400

    feat: customer card and grid

--- client/src/app/features/manager/customers/components/customer-card/customer-card.component.ts
+++ client/src/app/features/manager/customers/components/customer-card/customer-card.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { DivisorComponent } from '@components/divisor/divisor.component';
 
 @Component({
   selector: 'app-customer-card',
-  imports: [],
+  imports: [DivisorComponent],
   templateUrl: './customer-card.component.html',
-  styleUrl: './customer-card.component.css'
+  styleUrl: './customer-card.component.css',
 })
-export class CustomerCardComponent {
-
-}
+export class CustomerCardComponent {}
--- client/src/app/features/manager/customers/components/customers-grid/customers-grid.component.ts
+++ client/src/app/features/manager/customers/components/customers-grid/customers-grid.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CustomerCardComponent } from '../customer-card/customer-card.component';
 
 @Component({
   selector: 'app-customers-grid',
-  imports: [],
+  imports: [CustomerCardComponent],
   templateUrl: './customers-grid.component.html',
   styleUrl: './customers-grid.component.css',
 })
