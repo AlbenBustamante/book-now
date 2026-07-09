792c08f98a245c21542533f3c75694d27f46dccf
Alben Bustamante <albenbustamante@gmail.com>
2026-07-09 15:01:13 -0400

    feat: card component

--- client/src/app/features/manager/customers/components/customer-card/customer-card.component.ts
+++ client/src/app/features/manager/customers/components/customer-card/customer-card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { DivisorComponent } from '@components/divisor/divisor.component';
+import { CardComponent } from '@components/card/card.component';
 
 @Component({
   selector: 'app-customer-card',
-  imports: [DivisorComponent],
+  imports: [DivisorComponent, CardComponent],
   templateUrl: './customer-card.component.html',
   styleUrl: './customer-card.component.css',
 })
