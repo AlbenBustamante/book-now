c5fc72f7dd8e6db0ffd90351613c3ac93329f9b0
Alben Bustamante <albenbustamante@gmail.com>
2026-07-21 16:30:34 -0400

    design: appointments history table

--- client/src/app/features/manager/customers/customer-details/customer-details.component.ts
+++ client/src/app/features/manager/customers/customer-details/customer-details.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { DataCardComponent } from './components/data-card/data-card.component';
+import { AppointmentHistoryComponent } from './components/appointment-history/appointment-history.component';
 
 @Component({
   selector: 'app-customer-details',
-  imports: [DataCardComponent],
+  imports: [DataCardComponent, AppointmentHistoryComponent],
   templateUrl: './customer-details.component.html',
   styleUrl: './customer-details.component.css',
 })
