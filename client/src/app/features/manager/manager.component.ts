1bd8d7f4b863cdccf7e08a1af0acaaa8b62050fa
Alben Bustamante <albenbustamante@gmail.com>
2026-07-21 17:04:24 -0400

    update: footer layout, design, links

--- client/src/app/features/manager/manager.component.ts
+++ client/src/app/features/manager/manager.component.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from '@layouts/main-layout/components/footer/footer.component';
+import { FooterComponent } from '@components/footer/footer.component';
 
 @Component({
   selector: 'app-manager',
