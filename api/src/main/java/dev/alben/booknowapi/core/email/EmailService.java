60d658e006f64198e9fe7496bb378e33ef9e0d07
Alben Bustamante <albenbustamante@gmail.com>
2026-07-29 00:12:12 -0400

    update: email template styles

--- api/src/main/java/dev/alben/booknowapi/core/email/EmailService.java
+++ api/src/main/java/dev/alben/booknowapi/core/email/EmailService.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+
 @Service
 @RequiredArgsConstructor
 public class EmailService implements SendHtmlEmailUseCase {
@@ -20,20 +23,22 @@ public class EmailService implements SendHtmlEmailUseCase {
     @Override
     public void sendHtmlEmail(String to, String subject, String title, String body) {
         final var message = javaMailSender.createMimeMessage();
+        final var date = Instant.now();
+        final var year = DateTimeFormatter.ofPattern("yyyy").format(date);
 
         try {
             message.setFrom(new InternetAddress(from));
             message.setRecipients(MimeMessage.RecipientType.TO, to);
             message.setSubject(subject);
 
-            final var content = "<div style='padding: 12px 20px; background-color: #eee; border-radius: 25px;'>" +
-                    "<h1 style='font-weight: 600; font-size: 20px; text-align: center; color: #555; width: 100%;'>" +
+            final var content = "<div style='padding: 12px 20px; background-color: #00b8db; border-radius: 25px;'>" +
+                    "<h1 style='font-weight: 600; font-size: 20px; text-align: center; color: #fff; width: 100%;'>" +
                     title +
                     "</h1>" +
-                    "<div style='margin: 12px 0; font-size: 18px; color: #666'>" +
+                    "<div style='margin: 12px 0; font-size: 18px; color: #fff'>" +
                     body +
                     "</div>" +
-                    "<footer style='font-weight: 600; font-size: 16px; text-align: center; color: #777;'>BookNow &copy; 2025</footer>" +
+                    "<footer style='font-weight: 600; font-size: 16px; text-align: center; color: #fff;'>BookNow &copy; 2025 - " + year + "</footer>" +
                     "</div>";
 
             message.setContent(content, "text/html; charset=utf-8");
