1f6d26f59612cb5bbb7010da0788be8aad1d7344
Alben Bustamante <albenbustamante@gmail.com>
2026-07-29 04:00:20 -0400

    define: logic to load an image and display it to preview

--- client/src/app/features/manager/services/new-service/components/new-service-gallery/new-service-gallery.component.ts
+++ client/src/app/features/manager/services/new-service/components/new-service-gallery/new-service-gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { NewServiceCardComponent } from '../new-service-card/new-service-card.component';
 
 @Component({
@@ -7,4 +7,25 @@ import { NewServiceCardComponent } from '../new-service-card/new-service-card.co
   templateUrl: './new-service-gallery.component.html',
   styleUrl: './new-service-gallery.component.css',
 })
-export class NewServiceGalleryComponent {}
+export class NewServiceGalleryComponent {
+  readonly selectedFile = signal<File | undefined>(undefined);
+  readonly url = computed(() => {
+    const selectedFile = this.selectedFile();
+
+    if (!selectedFile) {
+      return 'https://www.svgrepo.com/show/508699/landscape-placeholder.svg';
+    }
+
+    const objectUrl = URL.createObjectURL(this.selectedFile()!);
+    return objectUrl;
+  });
+
+  onChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const files = target.files;
+
+    if (files && files.length >= 1) {
+      this.selectedFile.set(files[0]);
+    }
+  }
+}
