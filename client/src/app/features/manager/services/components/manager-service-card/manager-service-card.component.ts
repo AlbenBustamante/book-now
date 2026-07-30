1317170b6261c10d89aa3bddd9472aeca6faaf71
Alben Bustamante <albenbustamante@gmail.com>
2026-07-30 04:54:43 -0400

    feat: services cards consumes data from store

--- client/src/app/features/manager/services/components/manager-service-card/manager-service-card.component.ts
+++ client/src/app/features/manager/services/components/manager-service-card/manager-service-card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { CardComponent } from '@components/card/card.component';
 import { DivisorComponent } from '@components/divisor/divisor.component';
 import { ButtonComponent } from '@components/button/button.component';
+import { ServiceModel } from '@core/models/service.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-manager-service-card',
-  imports: [CardComponent, DivisorComponent, ButtonComponent],
+  imports: [CardComponent, DivisorComponent, ButtonComponent, CurrencyPipe],
   templateUrl: './manager-service-card.component.html',
   styleUrl: './manager-service-card.component.css',
 })
-export class ManagerServiceCardComponent {}
+export class ManagerServiceCardComponent {
+  readonly service = input.required<ServiceModel>();
+}
